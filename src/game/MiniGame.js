a51adedac4cc03542b3e63c90357584fe7d529ef
Ricardo Quesada <ricardoquesada@gmail.com>
2015-02-13 14:33:56 -0800

    clicking tiles

--- src/game/MiniGame.js
+++ src/game/MiniGame.js
@@ -1,5 +1,48 @@
 var MiniGame = cc.Node.extend({
 
+	ctor:function () {
+
+		this._super();
+
+		// set tile size, for future reference
+		var tile = new cc.Sprite(res.Tile_normal_png);
+		this.tileSize = tile.getContentSize();
+		this.tiles = [];
+		this.ROWS = 5;
+		this.COLS = 5;
+		
+		if ('mouse' in cc.sys.capabilities) {
+			cc.eventManager.addListener({
+				event: cc.EventListener.MOUSE,
+				onMouseDown: function(event) {
+					var pos = event.getLocation();
+					var target = event.getCurrentTarget();
+//					if (event.getButton() === cc.EventMouse.BUTTON_RIGHT)
+//						cc.log("onRightMouseDown at: " + pos.x + " " + pos.y );
+//					else if(event.getButton() === cc.EventMouse.BUTTON_LEFT)
+//						cc.log("onLeftMouseDown at: " + pos.x + " " + pos.y );
+					
+					var localPos = target.convertToNodeSpace(pos);
+					var tile_x = Math.floor(localPos.x / target.tileSize.width);
+					var tile_y = Math.floor(localPos.y / target.tileSize.height);
+					var tile_idx = tile_x + target.COLS * tile_y;
+					var spr = target.tiles[tile_idx];
+					spr.setVisible(!spr.isVisible());
+				},
+//				onMouseMove: function(event){
+//					var pos = event.getLocation(), target = event.getCurrentTarget();
+//					cc.log("onMouseMove at: " + pos.x + " " + pos.y );
+//				},
+//				onMouseUp: function(event){
+//					var pos = event.getLocation(), target = event.getCurrentTarget();
+//					cc.log("onMouseUp at: " + pos.x + " " + pos.y );
+//				}
+			}, this);
+		} else {
+			cc.log("MOUSE Not supported");
+		}
+	},
+
   onEnter:function() {
       this._super();
       this.createBoard();
@@ -11,15 +54,15 @@ var MiniGame = cc.Node.extend({
   },
 
   createBoard:function() {
-      for (var y=0; y<5; y++) {
-        for (var x=0; x<5; x++) {
-          var tile = new cc.Sprite(res.Tile_highlight_png);
-          var tileWidth = tile.getContentSize().width;
-          var tileHeight = tile.getContentSize().height;
+      for (var y=0; y<this.ROWS; y++) {
+        for (var x=0; x<this.COLS; x++) {
+          var tile = new cc.Sprite(res.Tile_normal_png);
 
           this.addChild(tile);
           tile.setAnchorPoint(0,0);
-          tile.setPosition(x * tileWidth, y * tileHeight);
+          tile.setPosition(x * this.tileSize.width, y * this.tileSize.height);
+          
+          this.tiles.push(tile);
         }
       }
   }
