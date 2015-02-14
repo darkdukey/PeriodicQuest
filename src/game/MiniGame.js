a731523cb830eabeee6ddedbdd0a84db707a90c4
Ricardo Quesada <ricardoquesada@gmail.com>
2015-02-13 16:11:08 -0800

    drag & drop support for building the word

--- src/game/MiniGame.js
+++ src/game/MiniGame.js
@@ -4,43 +4,53 @@ var MiniGame = cc.Node.extend({
 
 		this._super();
 
-		// set tile size, for future reference
+		// constants
 		var tile = new cc.Sprite(res.Tile_normal_png);
 		this.tileSize = tile.getContentSize();
-		this.tiles = [];
 		this.ROWS = 5;
 		this.COLS = 5;
+
+		// variables
+		this.tiles = [];
+		this.currentTileIdx = -1;
+		this.previousTileIdx = -1;
+		this.currentCombination = [];
 		
-		if ('mouse' in cc.sys.capabilities) {
-			cc.eventManager.addListener({
-				event: cc.EventListener.MOUSE,
-				onMouseDown: function(event) {
+		cc.eventManager.addListener({
+			event: cc.EventListener.MOUSE,
+
+			onMouseMove: function(event) {
+				if (event.getButton() === cc.EventMouse.BUTTON_LEFT) {
+					
 					var pos = event.getLocation();
 					var target = event.getCurrentTarget();
-//					if (event.getButton() === cc.EventMouse.BUTTON_RIGHT)
-//						cc.log("onRightMouseDown at: " + pos.x + " " + pos.y );
-//					else if(event.getButton() === cc.EventMouse.BUTTON_LEFT)
-//						cc.log("onLeftMouseDown at: " + pos.x + " " + pos.y );
-					
+						
 					var localPos = target.convertToNodeSpace(pos);
 					var tile_x = Math.floor(localPos.x / target.tileSize.width);
 					var tile_y = Math.floor(localPos.y / target.tileSize.height);
 					var tile_idx = tile_x + target.COLS * tile_y;
-					var spr = target.tiles[tile_idx];
-					spr.setVisible(!spr.isVisible());
-				},
-//				onMouseMove: function(event){
-//					var pos = event.getLocation(), target = event.getCurrentTarget();
-//					cc.log("onMouseMove at: " + pos.x + " " + pos.y );
-//				},
-//				onMouseUp: function(event){
-//					var pos = event.getLocation(), target = event.getCurrentTarget();
-//					cc.log("onMouseUp at: " + pos.x + " " + pos.y );
-//				}
-			}, this);
-		} else {
-			cc.log("MOUSE Not supported");
+					target.tileHighlighted(tile_idx);
+				}
+			},
+			onMouseUp: function(event) {
+				var target = event.getCurrentTarget();
+				target.endWord();
+			},
+		}, this);
+	},
+	
+	tileHighlighted:function(tile_idx) {
+		var idx = this.currentCombination.indexOf(tile_idx);
+		if (idx === -1) {
+			var spr = this.tiles[tile_idx];
+			spr.setTexture(res.Tile_highlight_png);
+			this.currentCombination.push(tile_idx);
 		}
+	},
+	endWord: function() {
+		for (var i=0; i<this.ROWS*this.COLS; i++)
+			this.tiles[i].setTexture(res.Tile_normal_png);
+		this.currentCombination = [];
 	},
 
   onEnter:function() {
