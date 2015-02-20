910a1eddd9253446c006f1ccc65b98530c6e2e09
Ricardo Quesada <ricardoquesada@gmail.com>
2015-02-20 14:15:21 -0800

    Element definitions

--- src/game/Element.js
+++ src/game/Element.js
@@ -1,10 +1,11 @@
 // A node that draws an Element inside a tile
 var Element = cc.Node.extend({
 
-    ctor:function (elementName) {
+    ctor:function (elementNumber) {
 		//
+        this._elementNumber = elementNumber;
+
 		this._super();
-        this._name = elementName;
         this._background = new cc.Sprite(res.Tile_normal_png);
         this._background.setAnchorPoint(0, 0);
 		this.addChild(this._background);
@@ -12,16 +13,17 @@ var Element = cc.Node.extend({
         var size = this._background.getContentSize();
         this.setContentSize(size);
 
-        this._name = new cc.LabelTTF(elementName, "Arial", size.width*0.7);
+        var name = ElementsProperties[elementNumber].name;
+        this._name = new cc.LabelTTF(name, "Arial", size.width*0.5);
         this._name.setAnchorPoint(0.5, 0.5);
-        this._name.setNormalizedPosition(0.5, 0.5);
+        this._name.setNormalizedPosition(0.5, 0.4);
         this._name.setColor({r:32, g:32, b:32});
         this.addChild(this._name);
 
-        this._number = new cc.LabelTTF("1", "Arial", size.width*0.2);
+        this._number = new cc.LabelTTF(ElementsProperties[elementNumber].number, "Arial", size.width*0.2);
         this._number.setAnchorPoint(0.5, 0.5);
-        this._number.setNormalizedPosition(0.15, 0.75);
-        this._number.setColor({r:64, g:64, b:64});
+        this._number.setNormalizedPosition(0.15, 0.74);
+        this._number.setColor({r:128, g:64, b:64});
         this.addChild(this._number);
 
     },
